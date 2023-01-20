87aace96696eff718d17e9e59801418137407bde
Iiro Partanen <iispar@utu.fi>
2023-01-20 17:06:58 +0200

    calls for top5 stations and average distance of station

--- controllers/bikes.js
+++ controllers/bikes.js
@@ -39,4 +39,76 @@ bikesRouter.get('/count', async (request, response) => {
   response.json(result)
 })
 
+/**
+ * Method for API call that gets the average distance from departure station (from station).
+ */
+bikesRouter.get('/average/departure/:id/:month', async (request, response) => {
+  const id = request.params.id
+  let month = request.params.month
+  let result = ''
+  if (month === 'all') {
+    result = await Bike.aggregate([
+      { $match: {'Departure_station_id': id }},
+      { $group: {_id: null, average: { $avg: '$Covered_distance'}}}
+    ])
+  } else {
+    month = parseInt(month)
+    result = await Bike.aggregate([
+      { $addFields: { 'Month' : { $month: '$Departure' }}},
+      { $match: {'$and': [{'Departure_station_id': id}, {'Month': month}]}},
+      { $group: {_id: null, average: { $avg: '$Covered_distance'}}}
+    ])
+  }
+  response.json(result)
+})
+
+/**
+ * Method for API call that gets the average distance from return station (to station).
+ */
+bikesRouter.get('/average/return/:id/:month', async (request, response) => {
+  const id = request.params.id
+  let month = request.params.month
+  let result = ''
+  if (month === 'all') {
+    result = await Bike.aggregate([
+      { $match: {'Return_station_id': id }},
+      { $group: {_id: null, average: { $avg: '$Covered_distance'}}}
+    ])
+  } else {
+    month = parseInt(month)
+    result = await Bike.aggregate([
+      { $addFields: { 'Month' : { $month: '$Departure' }}},
+      { $match: {'$and': [{'Return': id}, {'Month': month}]}},
+      { $group: {_id: null, average: { $avg: '$Covered_distance'}}}
+    ])
+  }
+  response.json(result)
+})
+
+/**
+ * Return a list of 5 most common stations that are visited from parameter station
+ */
+bikesRouter.get('/top/departure/:id', async (request, response) => {
+  const id = request.params.id
+  const result = await Bike.aggregate([
+    { $match: {'Departure_station_id': id}},
+    { $sortByCount: '$Return_station_id'},
+    { $limit: 5}
+  ])
+  response.json(result)
+})
+
+/**
+ * Return a list of 5 most common stations that users come from to this station.
+ */
+bikesRouter.get('/top/return/:id', async (request, response) => {
+  const id = request.params.id
+  const result = await Bike.aggregate([
+    { $match: {'Return_station_id': id}},
+    { $sortByCount: '$Departure_station_id'},
+    { $limit: 5}
+  ])
+  response.json(result)
+})
+
 module.exports = bikesRouter
